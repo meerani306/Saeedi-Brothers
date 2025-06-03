ced89a2461816ae79273099654264cdf04d05ecd
Muhammad Amjad <meerani306@gmail.com>
2025-06-03 23:43:45 +0500

    all files added and updated

--- script.js
+++ script.js
@@ -1,22 +1,29 @@
+var card_number=0;
+
+
 document.addEventListener('DOMContentLoaded', () => {
     fetch('data.json')
         .then(response => response.json())
-
         .then(data => {
-            
-            
+            console.log(data);
+
             let index = 0;
             setInterval(() => {
-                document.getElementById('abu').src = data.boss[index];
-                index = (index + 1) % data.boss.length;
+                document.getElementById('abu').src = data.abu[index];
+                index = (index + 1) % data.abu.length; // FIXED THIS LINE
             }, 2000);
-
-
-
         })
         .catch(error => console.error('Error loading profiles:', error));
 });
 
 
+const cards = document.querySelectorAll('.profile-card');
 
-
+cards.forEach((card) => {
+  card.addEventListener('click', () => {
+    const cardId = card.id;             // e.g., "card3"
+    const personId = cardId.slice(-1);  // Get "3"
+    localStorage.setItem('selectedPersonId', personId);
+    window.location.href = 'portfolio.html';
+  });
+});
